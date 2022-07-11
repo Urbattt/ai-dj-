bc83609450303bf18ba81f97db5be27ed9d319e9
Urbattt <80606638+Urbattt@users.noreply.github.com>
2022-07-10 22:41:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ leftWristy="";
 leftWristx="";
 rightWristy="";
 rightWristx="";
+scoreLeftWrist=0;
+scoreRightWrist=0;
+statusofsongleft="";
+statusofsongright=";"
 
 function preload(){
     song=loadSound("Sofia.mp3");
@@ -27,7 +31,35 @@ function preload(){
         
         function draw(){
         image(video,0,0,400,400);
-        
+        statusofsongleft=song.isPlaying();
+
+        if(scoreLeftWrist>0.2){
+            fill("#008000");
+            stroke("#FF0000");
+            circle(leftWristx,leftWristy,20);
+         song2.stop();
+
+         if(statusofsongleft=true){
+            song.play();
+document.getElementById("name").innerHTML="sofia the first";
+            }
+            }
+
+            statusofsongright=song2.isPlaying();
+
+            if(scoreRightWrist<0.2){
+                fill("#008000");
+                stroke("#FF0000");
+                circle(rightWristx,rightWristy,20);
+             song.stop();
+
+             if(statusofsongright=true){
+                song2.play();
+    document.getElementById("name").innerHTML="whatsapp theme";
+                }
+                }
+    
+               
         }
         
         function gotPoses(results){
@@ -39,7 +71,10 @@ function preload(){
         rightWristx=results[0].pose.rightWrist.x;
         rightWristy=results[0].pose.rightWrist.y;
         console.log("rightWristx= "+rightWristx+"rightWristy= "+rightWristy);
-        
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist= "+scoreLeftWrist);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist= "+scoreLeftWrist);
         }
         
         }
